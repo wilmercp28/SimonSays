63ab0c54fc68a1d9cadf7f5230a9d5fc5abe330e
wilme <wilmercp28@gmail.com>
2023-07-30 22:22:14 -0400

    New Build

--- app/src/main/java/com/example/simonsays/MainActivity.kt
+++ app/src/main/java/com/example/simonsays/MainActivity.kt
@@ -1,44 +1,55 @@
 package com.example.simonsays
 
+import android.graphics.Paint.Align
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewModelScope
+import androidx.compose.ui.unit.sp
 import com.example.simonsays.ui.theme.SimonSaysTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.util.Timer
+import kotlin.concurrent.scheduleAtFixedRate
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -48,22 +59,45 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    if (!gameLose.value){
                     SimonSaysGame()
-
+                } else{
+                    LosingScreen()
+                }
                 }
             }
         }
     }
 }
+var gameLose = mutableStateOf(false)
+var level = 1
+var clickCounter = 0
+val isSimonTurn =  mutableStateOf(true)
+var timer by mutableStateOf(10)
 
 @Preview
 @Composable
 fun SimonSaysGame() {
-    val isSimonTurn = remember { mutableStateOf(true) }
     val boxColors = remember { mutableStateMapOf<Int, Color>() }
-    var level = 1
-    var clickCounter = 0
-    LaunchedEffect(isSimonTurn.value) {
+
+
+    DisposableEffect(Unit) {
+        val timerTask = Timer()
+        timerTask.scheduleAtFixedRate(0L, 1000L) {
+            if (!isSimonTurn.value && timer > 0) {
+                timer -= 1
+            }
+            if (timer == 0){
+                gameLose.value = true
+            }
+        }
+
+        onDispose {
+            timerTask.cancel()
+        }
+    }
+
+LaunchedEffect(isSimonTurn) {
         if (isSimonTurn.value) {
             SimonActions(boxColors) {
                 isSimonTurn.value = false // Simon's turn is complete, allow the player to click
@@ -73,39 +107,93 @@ fun SimonSaysGame() {
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+        Box(
+
+            modifier = Modifier
+                .size(100.dp)
+                .background(Color.Green, RoundedCornerShape(20.dp))
+                .border(5.dp, Color.Black, RoundedCornerShape(10)),
+            Alignment.Center
+        ){
+
+            Text(
+
+                text = "$level\nLevel",
+                color = Color.Black,
+                textAlign = TextAlign.Center,
+                fontSize = 30.sp
+
+            )
+
+        }
         repeat(3) { rowIndex ->
-            Row(modifier = Modifier) {
+            Row(
+                modifier = Modifier
+                    .padding(5.dp)
+            ) {
                 repeat(3) { columnIndex ->
                     val boxIndex = rowIndex * 3 + columnIndex
                     Box(
                         modifier = Modifier
+                            .padding(5.dp)
                             .size(100.dp)
-                            .background(color = boxColors[boxIndex] ?: Color.Green)
+                            .background(
+                                color = boxColors[boxIndex] ?: Color.Green,
+                                RoundedCornerShape(10)
+                            )
+                            .border(5.dp, Color.Black, RoundedCornerShape(10))
                             .clickable {
                                 if (!isSimonTurn.value) {
                                     BoxChangeColor(boxIndex, boxColors, true)
                                     clickCounter++
-                                    if (level == clickCounter){
-                                        level++
-                                        CheckforWinner()
-                                        SimonActions(boxColors){
-
-                                        }
+                                    if (level == clickCounter) {
+                                        CheckForWinner(boxColors)
+                                        isSimonTurn.value = true
                                     }
                                 }
-
-
                             }
                     )
                 }
             }
         }
+
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(Color.Green, RoundedCornerShape(20.dp))
+                .border(5.dp, Color.Black, RoundedCornerShape(10)),
+            Alignment.Center
+        ){
+            Text(text = "$timer",
+                textAlign = TextAlign.Center
+            )
+
+        }
     }
+
 }
+
 var playerList = mutableListOf<Int>()
 var simonList = mutableListOf<Int>()
+fun CheckForWinner(boxColors: MutableMap<Int, Color>) {
+    if(simonList == playerList){
+        simonList.clear()
+        playerList.clear()
+        level++
+        timer = 10
+        SimonActions(boxColors) {
+            clickCounter = 0
+            isSimonTurn.value = false
+        }
+    } else {
+        gameLose.value = true
+    }
+
+}
+
+
 fun BoxChangeColor(boxIndex: Int, boxColors: MutableMap<Int, Color>,playerClick: Boolean){
 
     if(playerClick){
@@ -126,7 +214,7 @@ fun BoxChangeColor(boxIndex: Int, boxColors: MutableMap<Int, Color>,playerClick:
 
     fun SimonActions(boxColors: MutableMap<Int, Color>, onSimonTurnComplete: () -> Unit) {
         CoroutineScope(Dispatchers.Main).launch {
-            repeat(5) {
+            repeat(level) {
                 delay(1000)
                 val boxIndex = Random.nextInt(9)
                 BoxChangeColor(boxIndex, boxColors,false)
@@ -135,6 +223,60 @@ fun BoxChangeColor(boxIndex: Int, boxColors: MutableMap<Int, Color>,playerClick:
         }
 
     }
+@Preview
+@Composable
+fun LosingScreen(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Box(
+            modifier = Modifier
+                .size(height = 100.dp, width = 300.dp)
+                .background(Color.Green, RoundedCornerShape(20.dp))
+                .border(5.dp, Color.Black, RoundedCornerShape(10))
+                .clickable {
+                    playerList.clear()
+                    simonList.clear()
+                    clickCounter = 0
+                    level = 1
+                    isSimonTurn.value = true
+                    timer = 10
+                    gameLose.value = false
+
+
+                },
+            Alignment.Center
+        ){
+            Text(text = "Restart",
+                textAlign = TextAlign.Center,
+                fontSize = 30.sp
+            )
+
+        }
+        Spacer(modifier = Modifier.size(20.dp))
+        Box(
+            modifier = Modifier
+                .size(height = 100.dp, width = 300.dp)
+                .background(Color.Green, RoundedCornerShape(20.dp))
+                .border(5.dp, Color.Black, RoundedCornerShape(10)),
+            Alignment.Center
+        ){
+            Text(text = "Main Menu",
+                textAlign = TextAlign.Center,
+                fontSize = 30.sp
+            )
+
+        }
+        
+    }
+
+
+}
 
 
 
